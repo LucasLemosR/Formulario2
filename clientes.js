f1b0801065ddf6e21e171f1fb3aac63efbe24fe2
LucasLemosR <105325775+LucasLemosR@users.noreply.github.com>
2022-11-23 18:25:56 -0300

    Add files via upload

---
+++ clientes.js
@@ -0,0 +1,13 @@
+const pessoas = [
+    { name: "Fabiana Araújo", age: 33 },
+    { name: "Gabriel Gomes", age: 25 },
+    { name: "Fernando Henrique", age: 17 },
+    { name: "Ana Luiza", age: 2 },
+    { name: "Geralda do Nascimento", age: 93 },
+    { name: "Miguel Souza", age: 70 },
+    { name: "Antonio Miguel", age: 69 },
+];
+
+pessoas.forEach((name, age) => {
+    console.log(name, age)
+});
